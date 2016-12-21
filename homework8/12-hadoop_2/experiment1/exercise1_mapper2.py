4f0b6cd04039dc3e5d91dc548b3ab45ce8280d5c
Faldict <legadoubledyan@gmail.com>
2016-12-21 19:58:45 +0800

    rename the files

--- homework8/12-hadoop_2/experiment1/exercise1_mapper2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-
-for line in sys.stdin:
-    line = line.strip()
-    alpha, length, count = line.split('\t')
-    print "%s\t%s" % (alpha, float(length) / float(count))
